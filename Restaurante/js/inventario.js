3565ad573ced8ce67caa3066548c2eda2e78eb93
Paula Gómez <paulasgomez.1601@gmail.com>
2026-07-16 16:52:50 -0500

    main

--- Restaurante/js/inventario.js
+++ Restaurante/js/inventario.js
@@ -3,3 +3,20 @@ let Nombre_del_producto = document.getElementById("Nombre_del_producto").value;
 let Cantidad = document.getElementById("Cantidad").value;
 let Marca = document.getElementById("Marca").value;
 let Precio = document.getElementById("Precio").value;
+
+function validarProducto() {
+    if (codigo === "" || Nombre_del_producto === "" || Cantidad === "" || Marca === "" || Precio === "") {
+        console.log("Todos los campos de producto son obligatorios. Por favor, completa el formulario.");
+    } 
+    else {
+        if (!soloNumeros.test(codigo)) {
+            console.log("El campo Código no puede contener letras ni caracteres especiales.");
+        }
+        else if (!soloNumeros.test(Cantidad)) {
+            console.log("El campo Cantidad no puede contener letras.");
+        }
+        else if (!soloNumeros.test(Precio)) {
+            console.log("El campo Precio no puede contener letras.");
+        }
+    }
+}
--- Restaurante/js/pedidos.js
+++ Restaurante/js/pedidos.js
@@ -4,3 +4,17 @@ let Nombre_usuario = document.getElementById("Nombre_usuario").value;
 let Cantidad = document.getElementById("Cantidad").value;
 let Modo_de_pago = document.getElementById("Modo_de_pago").value;
 let Telefono = document.getElementById("Telefono").value;
+
+function validarPedido() {
+    if (Producto === "" || Direccion === "" || Nombre_usuario === "" || Cantidad === "" || Modo_de_pago === "" || Telefono === "") {
+        console.log("Todos los campos son obligatorios. Por favor, completa el formulario.");
+    } 
+    else {
+        if (!soloNumeros.test(Cantidad)) {
+            console.log("El campo Cantidad no puede contener letras.");
+        }
+        else if (!soloNumeros.test(Telefono)) {
+            console.log("El campo Teléfono no puede contener letras.");
+        }
+    }
+}
